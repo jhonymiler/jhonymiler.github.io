af7f4b607ac9b6ea48dc859f072bab96b278ff16
Jhony <jonatasmiler@gmail.com>
2022-10-05 12:18:03 -0300

    alteação caminhos build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './build'
+  },
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
