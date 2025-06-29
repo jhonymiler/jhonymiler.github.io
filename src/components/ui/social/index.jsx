57ccf092cf049dc502fa29b0cac337033bc9429a
Jhony Miler <jonatasmiler@gmail.com>
2025-06-29 17:13:30 -0300

    Updates

--- src/components/ui/social/index.jsx
+++
@@ -1,9 +0,0 @@
-import cn from 'classnames'
-
-const Social = ({ className, children }) => {
-  return <ul className={cn(className, 'social-share d-flex liststyle')}>{children}</ul>
-}
-
-export { default as SocialLink } from './social-link'
-
-export default Social
--- src/components/ui/timeline/timeline.jsx
+++
@@ -1,8 +0,0 @@
-import cn from 'classnames'
-
-const Timeline = ({ children, className }) => {
-  return <div className={cn('experience-list', className)}>{children}</div>
-}
-
-
-export default Timeline
